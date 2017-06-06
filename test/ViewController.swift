a89aaa786426ef02a489ba006ab51a7d4c165bff
Amichay Giuili <amichayg@icloud.com>
2017-06-06 17:39:31 +0300

    Yay

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,17 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var label:UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.text="Yay"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
